333779adb087a9dd93b157658b95f6b839efbd98
nsoktab <nataliasokolowskaa@gmail.com>
2023-10-14 13:19:07 +0200

    Update Reactstate.jsx

--- src/components/Reactstate.jsx
+++ src/components/Reactstate.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Reactstate(){
     
-    const [input, setInput] = useState("");
 
     return(
         <section>
